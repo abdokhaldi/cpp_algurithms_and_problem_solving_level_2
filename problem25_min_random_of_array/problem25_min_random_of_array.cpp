8c943e79fbd6ad893f98d1f69acecfbc1cc07fb6
abdokhaldi <84716693+abdokhaldi@users.noreply.github.com>
2023-01-07 21:46:21 +0100

    Add files via upload

---
+++ problem25_min_random_of_array/problem25_min_random_of_array.cpp
@@ -0,0 +1,52 @@
+
+// write a program to fill an array 
+// with max size 100  
+// with random Numbers 
+// from 1 to 100 , Then print Min Number in Array
+
+#include <iostream>
+using namespace std;
+
+int RandomNumber(int From, int To) {
+    //Function to generate a random number
+    int randNum = rand() % (To - From + 1) + From; return randNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength) {
+    cout << "\nEnter number of elements:\n";
+    cin >> arrLength;
+    for (int i = 0; i < arrLength; i++)
+        arr[i] = RandomNumber(1, 100);
+}
+void printArray(int arr[100], int arrLength) {
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+
+}
+
+int MinNumberInArray(int arr[100], int arrLength) {
+   int Min = 0;
+   Min = arr[0];
+    for (int i = 0; i < arrLength; i++) {
+        if (arr[i] < Min) {
+            Min = arr[i];
+        }
+
+    }
+    return Min;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int arr[100], arrLength;
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+
+    cout << "elements of array is : ";
+    printArray(arr, arrLength);
+    cout << " \n the Min Number is : ";
+    cout << MinNumberInArray(arr, arrLength);
+
+}
+
